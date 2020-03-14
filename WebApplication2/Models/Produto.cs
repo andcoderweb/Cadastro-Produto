9a6f2a912d019cd471d1afe973f533bbb4226c25
andcoderweb <andersoncoderweb@gmail.com>
2020-03-14 11:22:57 -0300

    Crud Cadastro de Produtos ASP NET Core

---
+++ WebApplication2/Models/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+
+    [Table("Produto")]
+    public class Produto
+    {
+        [Display(Description ="Código")]
+        public int Id { get; set; }
+        [Display(Description = "Descrição do Produto")]
+        public string Descricao { get; set; }
+    }
+}
